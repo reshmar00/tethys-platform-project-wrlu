13ad2ada3821082a4537203886e7bc8035aa8fd8
reshma raghavan <u1430631@umail.utah.edu>
2025-12-08 20:18:56 -0700

    Initialized basic tethys app

---
+++ tethysapp-project_wrlu/tethysapp/project_wrlu/app.py
@@ -0,0 +1,17 @@
+from tethys_sdk.base import TethysAppBase
+
+
+class App(TethysAppBase):
+    """
+    Tethys app class for WRLU.
+    """
+    name = 'WRLU'
+    description = 'Water Related Land Use in Utah'
+    package = 'project_wrlu'  # WARNING: Do not change this value
+    index = 'home'
+    icon = f'{package}/images/icon.gif'
+    root_url = 'project-wrlu'
+    color = '#00e6e6'
+    tags = '"app","hydrology","water use"'
+    enable_feedback = False
+    feedback_emails = []
