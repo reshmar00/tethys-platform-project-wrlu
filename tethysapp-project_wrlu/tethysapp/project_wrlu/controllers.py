1de826947422b3ad0573ffdb8ef7470450c6885a
reshma raghavan <u1430631@umail.utah.edu>
2025-12-09 09:56:39 -0700

    Updated basemaps and added drawing options to MapView

--- tethysapp-project_wrlu/tethysapp/project_wrlu/controllers.py
+++ tethysapp-project_wrlu/tethysapp/project_wrlu/controllers.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from tethys_sdk.routing import controller
-from tethys_sdk.gizmos import Button, MapView, MVView, MVLayer
+from tethys_sdk.gizmos import Button, MapView, MVView, MVDraw, MVLayer
 
 from django.urls import reverse_lazy
 from tethys_sdk.layouts import MapLayout
@@ -104,13 +104,26 @@ def home(request):
         legend_title='WRLU 1989–1999'
     )
 
+    basemaps = ['OpenStreetMap']
+
     # --------------------------
     # View Options
     # --------------------------
     view_options = MVView(
         projection='EPSG:4326',
-        center=[-111.0937, 39.3210],   # <-- Utah
-        zoom=7
+        center=[39.3, -111.9],   # <-- Utah center (latitude, longitude)
+        zoom=8,
+        maxZoom=18,
+        minZoom=2
+    )
+
+    # --------------------------
+    # Define drawing options
+    # --------------------------
+    drawing_options = MVDraw(
+        controls=['Modify', 'Delete', 'Move', 'Point', 'LineString', 'Polygon', 'Box'],
+        initial='Point',
+        output_format='WKT'
     )
 
     # --------------------------
@@ -119,9 +132,12 @@ def home(request):
     my_map = MapView(
         height='500px',
         width='100%',
+        controls=['ZoomSlider', 'FullScreen'],
         layers=[map_layer],
         view=view_options,
-        controls=['ZoomSlider', 'FullScreen']
+        basemap=basemaps,
+        draw=drawing_options,
+        legend=True
     )
 
 
