80d87de5adbaabbbe8c687cbfd932d368ccbd52d
reshma raghavan <u1430631@umail.utah.edu>
2025-12-08 20:33:20 -0700

    Cleaned up code in controllers.py

--- tethysapp-project_wrlu/tethysapp/project_wrlu/controllers.py
+++ tethysapp-project_wrlu/tethysapp/project_wrlu/controllers.py
@@ -1,49 +1,116 @@
+import os
+from dotenv import load_dotenv
 from tethys_sdk.routing import controller
 from tethys_sdk.gizmos import Button, MapView, Layer
 from .app import App
-import boto3
-from botocore.exceptions import NoCredentialsError
+
+# --------------------------
+# Load .env file
+# --------------------------
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=env_path)
+
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+BUCKET_KEY = os.getenv('BUCKET_KEY')
+
+# Build GeoJSON URL (public S3)
+geojson_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{BUCKET_KEY}"
+
 
 @controller
 def home(request):
     """
-    Controller for the app home page.
+    Controller for the app home page with buttons and a map.
     """
-    # Buttons (your existing code)
-    save_button = Button(display_text='', name='save-button', icon='save', style='success')
-    edit_button = Button(display_text='', name='edit-button', icon='pen', style='warning')
-    remove_button = Button(display_text='', name='remove-button', icon='trash', style='danger')
-    previous_button = Button(display_text='Previous', name='previous-button')
-    next_button = Button(display_text='Next', name='next-button')
 
-    s3_client = boto3.client('s3')
+    # --------------------------
+    # Buttons
+    # --------------------------
+    save_button = Button(
+        display_text='',
+        name='save-button',
+        icon='save',
+        style='success',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Save'
+        }
+    )
 
-    try:
-        presigned_url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={
-                
-            },
-            ExpiresIn=3600  # 1 hour
-        )
-    except NoCredentialsError:
-        presigned_url = None
+    edit_button = Button(
+        display_text='',
+        name='edit-button',
+        icon='pen',
+        style='warning',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Edit'
+        }
+    )
 
+    remove_button = Button(
+        display_text='',
+        name='remove-button',
+        icon='trash',
+        style='danger',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Remove'
+        }
+    )
+
+    previous_button = Button(
+        display_text='Previous',
+        name='previous-button',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Previous'
+        }
+    )
+
+    next_button = Button(
+        display_text='Next',
+        name='next-button',
+        attributes={
+            'data-bs-toggle': 'tooltip',
+            'data-bs-placement': 'top',
+            'title': 'Next'
+        }
+    )
+
+    # --------------------------
+    # Map Layer
+    # --------------------------
     map_layer = Layer(
-        source=presigned_url,
-        layer_options={'style': {'color': 'blue', 'weight': 2, 'fillOpacity': 0.5}},
+        source=geojson_url,
+        layer_options={
+            'style': {
+                'color': 'blue',
+                'weight': 2,
+                'fillOpacity': 0.5
+            }
+        },
         legend_title='WRLU 1989–1999'
     )
 
-    # Create MapView
+    # --------------------------
+    # MapView
+    # --------------------------
     my_map = MapView(
-        height='400px',
+        height='500px',
         width='100%',
         layers=[map_layer],
         controls=['ZoomSlider', 'FullScreen'],
         attribution='Data: S3 GeoJSON'
     )
 
+    # --------------------------
+    # Context
+    # --------------------------
     context = {
         'save_button': save_button,
         'edit_button': edit_button,
